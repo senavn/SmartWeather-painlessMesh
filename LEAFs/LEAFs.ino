e3d25240f218c14b8baa7545e301ebd0ed0fe665
Vinícius Sena <vinicius.drum@hotmail.com>
2021-03-29 19:21:50 -0300

    atualização

--- LEAFs/LEAFs.ino
+++ LEAFs/LEAFs.ino
@@ -165,30 +165,3 @@ void nodeTimeAdjustedCallback(int32_t offset) {
 void delayReceivedCallback(uint32_t from, int32_t delay) {
   Serial.printf("Delay to node %u is %d us\n", from, delay);
 }
-
-// Gerador de números randomicos
-int Randoms(int lower, int upper)
-{
-    int num = (rand() %
-               (upper - lower + 1)) +
-              lower;
-    return num;
-}
-
-//Método que devole cada posição de um vetor resultante de Split
-String splitStringByIndex(String data, char separator, int index)
-{
-  int found = 0;
-  int strIndex[] = {0, -1};
-  int maxIndex = data.length()-1;
-
-  for(int i=0; i<=maxIndex && found<=index; i++){
-    if(data.charAt(i)==separator || i==maxIndex){
-        found++;
-        strIndex[0] = strIndex[1]+1;
-        strIndex[1] = (i == maxIndex) ? i+1 : i;
-    }
-  }
-
-  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
-}
